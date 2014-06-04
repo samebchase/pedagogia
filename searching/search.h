7d1b128d2891b4deb422227f225dbfa841ffde1b
Samuel Chase <samebchase@gmail.com>
2014-06-05 00:47:36 +0530

    Clean slate

--- searching/search.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../arrays/array.h"
-
-int linear_search(int *array, size_t length, int element);
-
-int linear_search_rec(int *array, size_t length, int element);
-
-int binary_search(int *array, int element, size_t start, size_t end);
